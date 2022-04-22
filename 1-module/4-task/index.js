f969a7256427e44415715c06551c9832a7db54f4
Kramushkaa <71790093+Kramushkaa@users.noreply.github.com>
2022-04-22 17:57:00 +0300

    Решены задачи модуля 1

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,5 @@
 function checkSpam(str) {
-  // ваш код...
+  strLower = str.toLowerCase();
+  if (strLower.indexOf('1xbet') != -1 || strLower.indexOf('xxx') != -1) return true
+  else return false
 }
