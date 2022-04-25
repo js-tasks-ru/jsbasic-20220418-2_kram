7e97b67c0e9641514cfe16a7ee8a7cbe8b3da063
Kramushkaa <71790093+Kramushkaa@users.noreply.github.com>
2022-04-26 00:13:08 +0300

    исправлено решение после прохождения автотеста для задач модуля 3

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,7 +1,7 @@
 function showSalary(users, age) {
   let appropriateUsers = [];
   for (const user of users){
-    if (user.age < age){
+    if (user.age <= age){
       appropriateUsers.push([user.name,user.balance].join(", "));
     }
   }
