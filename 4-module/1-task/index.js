8172e9d49b1e174ac0f3b1336f5d6722da222b09
Kramushkaa <71790093+Kramushkaa@users.noreply.github.com>
2022-05-03 03:54:39 +0300

    решены задачи модуля 4, буду признателен более подробному Code Review для задачи 3

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
 function makeFriendsList(friends) {
-  // ваш код...
+
+  const ul = document.createElement("ul")
+
+  let template = "";
+  for (friend of friends) {
+    template += `<li> ${friend.firstName} ${friend.lastName} </li>`
+  }
+
+  ul.innerHTML = template;
+  return ul;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,41 @@
 function highlight(table) {
-  // ваш код...
+  // Дополнительно, упражнения для, сделал проверку на то, какую колонку мы берём. Теперь их можно поменять местами, но при этом должно работать :)
+  let cellNamesObj = [...table.querySelector("thead tr").children];
+  let cellNamesList = [];
+  for (let i = 0; i < cellNamesObj.length; i++) {
+    cellNamesList.push(cellNamesObj[i].textContent.toLowerCase());
+  }
+
+  let tbodyChildren = table.querySelector("tbody").children;
+  for (row of tbodyChildren) {
+    for (let i = 0; i < row.children.length; i++) {
+      if (cellNamesList[i] == "gender") {
+        if (row.children[i].textContent == "m") {
+          row.classList.add("male")
+        }
+        if (row.children[i].textContent == "f") {
+          row.classList.add("female")
+        }
+      }
+
+      if (cellNamesList[i] == "age") {
+        if (Number(row.children[i].textContent) < 18) {
+          row.style.textDecoration = "line-through"
+        }
+        
+      }
+
+      if (cellNamesList[i] == "status") {
+        if (!row.children[i].dataset.available) {
+          row.hidden = true
+        }
+        else if (row.children[i].dataset.available == "true") {
+          row.classList.add("available")
+        }
+        else {
+          row.classList.add("unavailable")
+        }
+      }
+    }
+  }
 }
